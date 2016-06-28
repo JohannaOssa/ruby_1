9ac31064643d4b6844bb6c5991c45e2c26680b5d
JohannaOssa <johannaossa10@gmail.com>
2016-06-27 20:45:05 -0500

    agregue archivo initial_class y cree operations como ejercicio

---
+++ operations.rb
@@ -0,0 +1,21 @@
+class Operations
+  def self.suma(num1, num2)
+    num1 + num2
+  end
+
+  def self.resta(num1, num2)
+    num1 - num2
+  end
+
+  def self.multi(num1, num2)
+    num1 * num2
+  end
+
+  def self.division(num1, num2)
+    num1 / num2
+  end
+
+  def self.concatenar(let1, let2)
+    "#{let1}  #{let2}!"
+  end
+end
