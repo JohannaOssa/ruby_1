0030b52752475cef156882d1df71d98a0ddc7d71
JohannaOssa <johannaossa10@gmail.com>
2016-06-30 17:16:16 -0500

    anexo ejemplos array y hash. Desarrollo ejercicio personas.

---
+++ personas.rb
@@ -0,0 +1,6 @@
+class Personas
+  def self.name
+    personas = { '1' => { 'nombre' => 'Pedro', 'apellido' => 'Perez', 'cedula'=>
+       10147401321, 'beneficiarios' => [{ nombre: 'Ana', apellido: 'Rosales', cedula: 72218786}]}}
+  end
+end
